7a28a59b1ec86b14ce77294eafb3a0414c013230
devmarufurrahaman <mdmarufur5682@gmail.com>
2023-08-19 10:00:14 +0600

    image pick done

--- app/src/main/java/com/example/imagefromgallery/MainActivity.java
+++ app/src/main/java/com/example/imagefromgallery/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.imagefromgallery;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imgView;
+    Button galleryBtn;
+    private final int REQUEST_CODE_GALLERY = 102;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // id define
+        imgView = findViewById(R.id.imgView);
+        galleryBtn = findViewById(R.id.gallery);
+
+
+        galleryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent igallery = new Intent(Intent.ACTION_PICK);
+                igallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(igallery,REQUEST_CODE_GALLERY);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE_GALLERY){
+            imgView.setImageURI(data.getData());
+        }
     }
 }
